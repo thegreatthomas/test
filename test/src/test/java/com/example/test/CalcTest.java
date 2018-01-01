012ddf53823583a4c9a3f9dec0e7b10d7df589b9
yangjichao <thegreatthomas@163.com>
2018-01-01 19:30:33 +0800

    asda

---
+++ test/src/test/java/com/example/test/CalcTest.java
@@ -0,0 +1,22 @@
+package com.example.test;
+
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalcTest {
+	
+	@Test
+	public void doAdd() throws Exception{
+		CloseableHttpClient client = HttpClients.createDefault();
+		HttpGet request = new HttpGet("http://192.168.119.151:8080/test/CalcServlet?a=1&b=2");
+		CloseableHttpResponse response = client.execute(request);
+		String actual = EntityUtils.toString(response.getEntity());
+		Assert.assertEquals(actual, "3");
+	}
+
+}
